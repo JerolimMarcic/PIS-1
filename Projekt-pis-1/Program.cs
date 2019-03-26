96ef224d450c1ae82c5a3a270ed39ef1f7ae7bf4
JerolimMarcic <jerolim.marcic@gmail.com>
2019-03-26 18:55:41 +0100

    First commit

---
+++ Projekt-pis-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projekt_pis_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
